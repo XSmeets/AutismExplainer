cc1a70e4b9240b9d6b5663a684c4523906a8ee19
Xander Smeets <xander@smeets.re>
2025-02-17 22:17:12 +0100

    Initial Commit

---
+++ AutismExplainer/AutismExplainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutismExplainerApp.swift
+//  AutismExplainer
+//
+//  Created by Xander Smeets on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutismExplainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutismExplainerTests/AutismExplainerTests.swift
@@ -0,0 +1,16 @@
+//
+//  AutismExplainerTests.swift
+//  AutismExplainerTests
+//
+//  Created by Xander Smeets on 17/02/2025.
+//
+
+import Testing
+
+struct AutismExplainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
