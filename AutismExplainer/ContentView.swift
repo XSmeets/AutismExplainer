f814bb2d60f519bd63ffad6097216f4bf02575d1
Xander Smeets <xander@smeets.ee>
2025-02-18 21:43:53 +0100

    Update EnergyMeterView

--- AutismExplainer/ContentView.swift
+++ AutismExplainer/ContentView.swift
@@ -11,18 +11,23 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("This app attempts to explain autism to people who are unfamiliar with it. Choose any of the following topics to learn more about them.")
-                    .lineLimit(nil)
-//                    .multilineTextAlignment(.leading)
-                NavigationLink(destination: StimuliView()) {
-                    Text("Stimuli")
-                }
-                NavigationLink(destination: OverStimulationView()) {
-                    Text("Overstimulation")
-                }
-                Text("This app provides several tools for people with autism.")
-                NavigationLink(destination: EnergyMeterView()) {
-                    Text("Energy Meter")
+                Text("AutismExplainer")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                ScrollView {
+                    Text("This app attempts to explain autism to people who are unfamiliar with it. Choose any of the following topics to learn more about them.")
+                        .lineLimit(nil)
+                    //                    .multilineTextAlignment(.leading)
+                    NavigationLink(destination: StimuliView()) {
+                        Text("Stimuli")
+                    }
+                    NavigationLink(destination: OverStimulationView()) {
+                        Text("Overstimulation")
+                    }
+                    Text("This app provides several tools for people with autism.")
+                    NavigationLink(destination: EnergyMeterView()) {
+                        Text("Energy Meter")
+                    }
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
--- AutismExplainer/EnergyMeterView.swift
+++ AutismExplainer/EnergyMeterView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct EnergyMeterView: View {
+    @State private var energyLevel: Int = 1
+    
     var body: some View {
         Text("")
+        HStack {
+            TextField("Activity", text: .constant(""))
+            Stepper(value: $energyLevel, in: 1...10) {
+                Text("\(energyLevel)")
+            }
+        }
     }
 }
 
